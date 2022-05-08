7d29f159dfdbe708d507e3701895bd73e8048fde
lingtangg <60630600+lingtangg@users.noreply.github.com>
2022-05-08 12:54:00 +1000

    Prompt users to choose size of the grid

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const container = document.getElementsByClassName('container')[0];
+const gridSize = document.getElementById('grid-size');
+const maxWidth = 960;
+const maxHeight = 480;
 
-function makeGrid() {
-    let grids = 16;
+function makeGrid(grids) {
     for (let y = 1; y < grids + 1; y++) { // create rows
         const row = document.createElement('div');
         row.classList.add('rows');
@@ -13,6 +15,8 @@ function makeGrid() {
             const div = document.createElement('div');
             div.classList.add('grids');
             div.style.flexGrow = '1';
+            div.style.width = maxWidth / grids + "px"; // fix size regardless of grids
+            div.style.height = maxHeight / grids + "px";
             rows.appendChild(div);
             div.addEventListener("mouseover", () => { // hover state
                 div.classList.remove("grids");
@@ -22,8 +26,17 @@ function makeGrid() {
     }
 }
 
+function chooseGridSize() {
+    return parseInt(prompt("What size grid would you like?"));
+}
+
+function changeColours() {
+    
+}
+
+// Choose grid size through button
+gridSize.addEventListener("click", function () {
+    makeGrid(chooseGridSize());
+})
 
 
-window.onload = function() {
-    makeGrid();
-};
